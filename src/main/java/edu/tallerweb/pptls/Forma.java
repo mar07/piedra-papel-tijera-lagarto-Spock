63c71752c754dfb40461f53d0eca6a107f7cc2ab
unknown <m.martinez.0788@gmail.com>
2013-05-07 23:27:31 -0300

    trabajo numero uno

--- src/main/java/edu/tallerweb/pptls/Forma.java
+++ src/main/java/edu/tallerweb/pptls/Forma.java
@@ -8,13 +8,53 @@ public enum Forma {
 	PIEDRA(0), SPOCK(1), PAPEL(2), LAGARTO(3), TIJERA(4);
 
 	private final Integer valor;
+	
+	Resultado[][] resultados = new Resultado[5][5];
 
 	private Forma(final Integer valor) {
 		this.valor = valor;
+		
+	resultados[0][0] = Resultado.EMPATA;
+	resultados[1][1] = Resultado.EMPATA;
+	resultados[2][2] = Resultado.EMPATA;
+	resultados[3][3] = Resultado.EMPATA;
+	resultados[4][4] = Resultado.EMPATA;
+	
+	resultados[0][1] = Resultado.PIERDE;
+	resultados[0][2] = Resultado.PIERDE;
+	resultados[0][3] = Resultado.GANA;
+	resultados[0][4] = Resultado.GANA;
+	
+	resultados[1][0] = Resultado.GANA;
+	resultados[1][2] = Resultado.PIERDE;
+	resultados[1][3] = Resultado.PIERDE;
+	resultados[1][4] = Resultado.GANA;
+	
+	resultados[2][0] = Resultado.GANA;
+	resultados[2][1] = Resultado.GANA;
+	resultados[2][3] = Resultado.PIERDE;
+	resultados[2][4] = Resultado.PIERDE;
+	
+	resultados[3][0] = Resultado.PIERDE;
+	resultados[3][1] = Resultado.GANA;
+	resultados[3][2] = Resultado.GANA;
+	resultados[3][4] = Resultado.PIERDE;
+	
+	resultados[4][0] = Resultado.PIERDE;
+	resultados[4][1] = Resultado.PIERDE;
+	resultados[4][2] = Resultado.GANA;
+	resultados[4][3] = Resultado.GANA;
+	
 	}
 
 	public Integer getValor() {
 		return this.valor;
 	}
-
+	
+	public Resultado jugar(Forma forma){
+		return this.resultados[this.getValor()][forma.getValor()];
+	}
+	
 }
+
+
--- src/main/java/edu/tallerweb/pptls/Mano.java
+++ src/main/java/edu/tallerweb/pptls/Mano.java
@@ -4,6 +4,17 @@ package edu.tallerweb.pptls;
  * Representa una de las Manos involucradas en el juego
  */
 public class Mano {
+	
+		private Forma forma;
+		
+		public Forma getForma(){
+			return forma;
+		}
+		
+		public void setForma(Forma forma){
+			this.forma = forma;
+		}
+		
 
 	/**
 	 * Toda Mano debe crearse con una forma dada, que será
@@ -11,7 +22,7 @@ public class Mano {
 	 * @param forma, la Forma que adopta la Mano.
 	 */
 	public Mano(final Forma forma) {
-		throw new RuntimeException("No implementado aún");
+		this.setForma(forma);
 	}
 
 	/**
@@ -21,7 +32,7 @@ public class Mano {
 	 * @return un Resultado, de acuerdo al estado del juego.
 	 */
 	public Resultado jugarCon(final Mano otra) {
-		throw new RuntimeException("No implementado aún");
+		return this.getForma().jugar(otra.getForma());
 	}
 
 }
